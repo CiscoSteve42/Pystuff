8d3aa2bd4c7e315fb5296eec62530275d1a642e5
CiscoSteve42 <skoomasteve42@proton.me>
2024-07-12 09:34:47 -0500

    Updating Game Progress

--- SuperTux Python Adventures/main.py
+++ SuperTux Python Adventures/main.py
@@ -9,20 +9,21 @@ screen = pygame.display.set_mode((1200,800))
 pygame.display.set_caption('Super Tux Python World')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('fonts/Pixeltype.ttf', 69)
+game_active = True
 fps = 60
 
-pygame.mixer.music.load('instrumental.mp3')
-pygame.mixer.music.play()
+pygame.mixer.music.load('music/instrumental.mp3')
+pygame.mixer.music.play(-1)
 
-bg_surface = pygame.image.load('backgrounds/winter1.png').convert()
+bg_surface = pygame.image.load('images/backgrounds/winter1.png').convert()
 text_surface = test_font.render('Super Tux: The Python Adventures', False, 'Yellow')
 
-tux_surface = pygame.image.load('Tux/big/idle-0.png').convert_alpha()
-tux_rect = tux_surface.get_rect(midbottom = (50,750))
-
-snail_surface = pygame.image.load('snail/snail1.png').convert_alpha()
+snail_surface = pygame.image.load('images/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(bottomright = (1000,750))
 
+tux_surface = pygame.image.load('images/Tux/big/idle-0.png').convert_alpha()
+tux_rect = tux_surface.get_rect(midbottom = (50,750))
+tux_grav = 0
 
 while True:
     for event in pygame.event.get():
@@ -30,21 +31,42 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(bg_surface,(0,0))
-    screen.blit(tux_surface,tux_rect)
-    screen.blit(text_surface,(250,150))
-    screen.blit(snail_surface,snail_rect)
-    snail_rect.x -= 4
-    if snail_rect.right <= 0: snail_rect.left = 1200
+    if game_active:
+        screen.blit(bg_surface,(0,0))
+        screen.blit(text_surface,(250,150))
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
-        tux_rect.x -= 4
-    if keys[pygame.K_d]:
-        tux_rect.x += 4
+        snail_rect.x -= 4
+        if snail_rect.right <= 0: snail_rect.left = 1200
+        screen.blit(snail_surface,snail_rect)
 
-    if tux_rect.colliderect(snail_rect):
-        print('collision')
+        tux_grav += 1
+        tux_rect.y += tux_grav
+        if tux_rect.bottom >= 750: tux_rect.bottom = 750
+        
+        screen.blit(tux_surface,tux_rect)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            tux_rect.x -= 4
+        if keys[pygame.K_d]:
+            tux_rect.x += 4
+        if keys[pygame.K_SPACE] and tux_rect.bottom >= 750:
+            tux_grav = -20
+
+
+        if tux_rect.colliderect(snail_rect):
+            game_active = False
+            
+    else:
+        pygame.mixer.music.stop()
+        gameover_img = pygame.image.load('images/gameover/gameoverscreen.jpg')
+        screen.blit(gameover_img,(0,0))
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            game_active = True
+            snail_rect.left = 1200
+            tux_rect.left = 50
+            pygame.mixer.music.play(-1)
 
     pygame.display.update()
     clock.tick(fps)
